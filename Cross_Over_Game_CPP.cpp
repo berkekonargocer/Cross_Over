354eb4e14a1bfee1091e15bf16b62107b29b6cae
Berke Konargöçer <berke.konargocer@gmail.com>
2023-05-04 11:12:39 +0300

    Circle Created and Can Be Controlled

--- Cross_Over_Game_CPP.cpp
+++ Cross_Over_Game_CPP.cpp
@@ -1,10 +1,51 @@
-// Cross_Over_Game_CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "raylib.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	constexpr int WIDTH = 1024;
+	constexpr int HEIGHT = 768;
+
+	constexpr int START_POINT_X = 30;
+	constexpr int START_POINT_Y = 375;
+	constexpr float RADIUS = 25.0f;
+
+	int circleCurrentXPoint = 30;
+	int circleCurrentYPoint = 375;
+
+	InitWindow(WIDTH, HEIGHT, "Cross Over");
+
+
+	SetTargetFPS(144);
+
+	while (WindowShouldClose() == false)
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+
+		// ------------------------------------- GAME LOGIC START -------------------------------------
+
+		if (IsKeyDown(KEY_D) && circleCurrentXPoint < WIDTH - RADIUS)
+		{
+			circleCurrentXPoint += 3;
+		}
+		if (IsKeyDown(KEY_A)&& circleCurrentXPoint > 1 + RADIUS)
+		{
+			circleCurrentXPoint -= 3;
+		}
+		if (IsKeyDown(KEY_W) && circleCurrentYPoint > 1 + RADIUS)
+		{
+			circleCurrentYPoint -= 3;
+		}
+		if (IsKeyDown(KEY_S) && circleCurrentYPoint < HEIGHT - RADIUS)
+		{
+			circleCurrentYPoint += 3;
+		}
+
+		DrawCircle(circleCurrentXPoint, circleCurrentYPoint, RADIUS, PURPLE);
+
+		// ------------------------------------- GAME LOGIC END ---------------------------------------
+
+		EndDrawing();
+	}
 }
