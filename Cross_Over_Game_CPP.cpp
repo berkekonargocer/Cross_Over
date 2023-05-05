35eff872e11f2aa2e0d791102589e25ca09e1702
Berke Konargöçer <berke.konargocer@gmail.com>
2023-05-05 11:12:36 +0300

    Player Circle Collision Area | PlayerCircle class | Game class | Rectangle Object class
    Player Collision Area algorithm
    PlayerCircle, Game and RectangleObject classes are created
    Rectangle moves up and down

--- Cross_Over_Game_CPP.cpp
+++ Cross_Over_Game_CPP.cpp
@@ -1,23 +1,67 @@
 #include <iostream>
 #include "raylib.h"
 
+class PlayerCircle
+{
+public:
+	const int START_POSITION_X = 30;
+	const int START_POSITION_Y = 375;
+	const int RADIUS = 25.0f;
+
+	int currentXPosition = 30;
+	int currentYPosition = 375;
+};
+
+class Game
+{
+public:
+	const int SCREEN_WIDTH = 1024;
+	const int SCREEN_HEIGHT = 768;
+	int level = 1;
+};
+
+class RectangleObject
+{
+public:
+	int RectangleObjectPositionX = 120;
+	int RectangleObjectPositionY = 0;
+	int RectangleObjectWidth = 50;
+	int RectangleObjectHeight = 50;
+	int DirectionX = 1;
+	int DirectionY = 1;
+	int MoveSpeed = 10;
+	Color RectangleObjectColor{ RED };
+
+	RectangleObject() = default;
+
+	RectangleObject(int startPositionX, int startPositionY, const int width, const int height, int directionX, int directionY, int moveSpeed, const Color color)
+	{
+		RectangleObjectPositionX = startPositionX;
+		RectangleObjectPositionY = startPositionY;
+		RectangleObjectWidth = width;
+		RectangleObjectHeight = height;
+		DirectionX = directionX;
+		DirectionY = directionY;
+		MoveSpeed = moveSpeed;
+		RectangleObjectColor = color;
+	}
+};
+
 int main()
 {
-	constexpr int WIDTH = 1024;
-	constexpr int HEIGHT = 768;
 
-	constexpr int START_POINT_X = 30;
-	constexpr int START_POINT_Y = 375;
-	constexpr float RADIUS = 25.0f;
+	auto* game = new Game();
+	auto* playerCircle = new PlayerCircle();
+	const auto* rectangleObject = new RectangleObject[5];
 
-	int circleCurrentXPoint = 30;
-	int circleCurrentYPoint = 375;
+	RectangleObject rectangle1 = rectangleObject[0];
+	rectangle1.MoveSpeed = 12;
 
-	InitWindow(WIDTH, HEIGHT, "Cross Over");
-
+	InitWindow(game->SCREEN_WIDTH, game->SCREEN_HEIGHT, "Cross Over");
 
 	SetTargetFPS(144);
 
+	// ------------------------------------------ GAME LOOP START -------------------------------------
 	while (WindowShouldClose() == false)
 	{
 		BeginDrawing();
@@ -25,27 +69,56 @@ int main()
 
 		// ------------------------------------- GAME LOGIC START -------------------------------------
 
-		if (IsKeyDown(KEY_D) && circleCurrentXPoint < WIDTH - RADIUS)
-		{
-			circleCurrentXPoint += 3;
-		}
-		if (IsKeyDown(KEY_A)&& circleCurrentXPoint > 1 + RADIUS)
-		{
-			circleCurrentXPoint -= 3;
-		}
-		if (IsKeyDown(KEY_W) && circleCurrentYPoint > 1 + RADIUS)
-		{
-			circleCurrentYPoint -= 3;
-		}
-		if (IsKeyDown(KEY_S) && circleCurrentYPoint < HEIGHT - RADIUS)
-		{
-			circleCurrentYPoint += 3;
-		}
 
-		DrawCircle(circleCurrentXPoint, circleCurrentYPoint, RADIUS, PURPLE);
+		// ------------------------------- SET PLAYER CIRCLE COLLISION --------------------------------
+		const int circleUpCollision = playerCircle->currentYPosition - playerCircle->RADIUS;
+		const int circleDownCollision = playerCircle->currentYPosition + playerCircle->RADIUS;
+		const int circleRightCollision = playerCircle->currentXPosition + playerCircle->RADIUS;
+		const int circleLeftCollision = playerCircle->currentXPosition - playerCircle->RADIUS;
+		// --------------------------------------------------------------------------------------------
+
+
+		// ------------------------------------- MOVEMENT INPUTS --------------------------------------
+		if (IsKeyDown(KEY_D) && circleRightCollision < game->SCREEN_WIDTH)
+		{
+			playerCircle->currentXPosition += 3;
+		}
+		if (IsKeyDown(KEY_A) && circleLeftCollision > 0)
+		{
+			playerCircle->currentXPosition -= 3;
+		}
+		if (IsKeyDown(KEY_W) && circleUpCollision > 0)
+		{
+			playerCircle->currentYPosition -= 3;
+		}
+		if (IsKeyDown(KEY_S) && circleDownCollision < game->SCREEN_HEIGHT)
+		{
+			playerCircle->currentYPosition += 3;
+		}
+		// --------------------------------------------------------------------------------------------
+
+
+		// ------------------------------ MOVE RECTANGLE TO UP AND DOWN -------------------------------
+		rectangle1.RectangleObjectPositionY += rectangle1.DirectionY * rectangle1.MoveSpeed;
+
+		if (rectangle1.RectangleObjectPositionY > game->SCREEN_HEIGHT - rectangle1.RectangleObjectHeight || rectangle1.RectangleObjectPositionY < 0)
+		{
+			rectangle1.DirectionY = -rectangle1.DirectionY;
+		}
+		// --------------------------------------------------------------------------------------------
+
+
+		// -------------------------------------- DRAW OBJECTS ----------------------------------------
+		DrawRectangle(rectangle1.RectangleObjectPositionX, rectangle1.RectangleObjectPositionY,
+			rectangle1.RectangleObjectWidth, rectangle1.RectangleObjectHeight, rectangle1.RectangleObjectColor);
+
+		DrawCircle(playerCircle->currentXPosition, playerCircle->currentYPosition, playerCircle->RADIUS, PURPLE);
+		// --------------------------------------------------------------------------------------------
+
 
 		// ------------------------------------- GAME LOGIC END ---------------------------------------
 
 		EndDrawing();
 	}
+	// ----------------------------------------- GAME LOOP END ----------------------------------------
 }
